48f54c16c59f10f88a637b315fc1e1563e93017d
Atharv Yeolekar <atharv6f@gmail.com>
2021-01-01 04:03:47 +0400

    First Commit

---
+++ preload.js
@@ -0,0 +1,22 @@
+
+const { ipcRenderer, contextBridge } = require('electron');
+
+
+contextBridge.exposeInMainWorld('electron', {
+  notificationApi: {
+    sendNotification(message) {
+      ipcRenderer.send('notify', message);
+    }
+  },
+  appApi: {
+    quitApp() {
+      ipcRenderer.send('app-quit');
+    }
+  },
+  batteryApi: {
+
+  },
+  fileApi: {
+
+  }
+})
